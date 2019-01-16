493eb1da1e5723a58b9813bce338251dc73db96f
studentliying <540906886@qq.com>
2019-01-16 19:06:41 +0800

    try deployment

--- src/views/RoomPage/RoomPage.jsx
+++ src/views/RoomPage/RoomPage.jsx
@@ -18,41 +18,40 @@ import { Link } from "react-router-dom";
 // core components
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
 import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CardIcon from "components/Card/CardIcon.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
-import CustomInput from "components/CustomInput/CustomInput.jsx";
 import Table from "components/Table/Table.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
+import AddRoom from "../../components/Room/AddRoom";
 
 let rooms = [
   { location: "410", capacity: 10, status: 2, device: "是", others: "无" },
-  { location: "411", capacity: 15, status: 1, device: "是", others: "投影设备已坏" },
+  { location: "411", capacity: 15, status: 1, device: "是", others: "投影设备已坏"},
   { location: "412", capacity: 5, status: 0, device: "否", others: "无" },
   { location: "501", capacity: 5, status: 2, device: "是", others: "无" }
 ];
 
-function JSONToArray(jsonArray, type){
-    let re = [];
-    for (let i in jsonArray){
-        let ele = jsonArray[i];
-        let temp_ele = [];
-        let tmp = "";
-        temp_ele.push(ele.location);
-        temp_ele.push(ele.capacity);
-        if(ele.status == 2)
-            tmp = "使用中";
-        else
-            tmp = "空闲";
-        temp_ele.push(tmp);
-        temp_ele.push(ele.device);
-        temp_ele.push(ele.others);
-        re.push(temp_ele);
-    }
-    return re;
+function JSONToArray(jsonArray, type) {
+  let re = [];
+  for (let i in jsonArray) {
+    let ele = jsonArray[i];
+    let temp_ele = [];
+    let tmp = "";
+    temp_ele.push(ele.location);
+    temp_ele.push(ele.capacity);
+    if (ele.status == 2) tmp = "使用中";
+    else tmp = "空闲";
+    temp_ele.push(tmp);
+    temp_ele.push(ele.device);
+    temp_ele.push(ele.others);
+    re.push(temp_ele);
+  }
+  return re;
 }
 
 function roomCategory(capacity) {
@@ -138,53 +137,62 @@ class Dashboard extends React.Component {
                 {
                   tabName: "查询会议室",
                   tabIcon: Search,
-                  tabContent:
+                  tabContent: (
                     <GridContainer xs={12} sm={12} md={12}>
-                        <GridItem xs={12} sm={12} md={4} >
-                            <Button color="black">显示所有会议室</Button>
-                        </GridItem>
-                        <GridItem xs={12} sm={12} md={8}>
-                            <CustomInput
-                                formControlProps={{
-                                    className: classes.margin + " " + classes.search
-                                }}
-                                inputProps={{
-                                    placeholder: "输入与会议室相关信息",
-                                    inputProps: {
-                                        "aria-label": "Search"
-                                    }
-                                }}
-                                style={{width: '40%'}}
+                      <GridItem xs={12} sm={12} md={4}>
+                        <Button color="black">显示所有会议室</Button>
+                      </GridItem>
+                      <GridItem xs={12} sm={12} md={8}>
+                        <CustomInput
+                          formControlProps={{
+                            className: classes.margin + " " + classes.search
+                          }}
+                          inputProps={{
+                            placeholder: "输入与会议室相关信息",
+                            inputProps: {
+                              "aria-label": "Search"
+                            }
+                          }}
+                          style={{ width: "40%" }}
+                        />
+                        <Button color="white" aria-label="edit" justIcon round>
+                          <Search />
+                        </Button>
+                      </GridItem>
+                      <GridItem xs={12} sm={12} md={12}>
+                        <Card>
+                          <CardBody>
+                            <Table
+                              tableHeaderColor="primary"
+                              tableHead={[
+                                "房间号",
+                                "可容纳人数",
+                                "状态",
+                                "有无投影设备",
+                                "备注"
+                              ]}
+                              tableData={JSONToArray(rooms, "rooms")}
                             />
-                            <Button color="white" aria-label="edit" justIcon round>
-                                <Search />
-                            </Button>
-                        </GridItem>
-                        <GridItem xs={12} sm={12} md={12}>
-                            <Card>
-                                <CardBody>
-                                    <Table
-                                        tableHeaderColor="primary"
-                                        tableHead={["房间号","可容纳人数", "状态", "有无投影设备", "备注"]}
-                                        tableData={JSONToArray(rooms, "rooms")}
-                                    />
-                                </CardBody>
-                            </Card>
-                        </GridItem>
+                          </CardBody>
+                        </Card>
+                      </GridItem>
                     </GridContainer>
+                  )
                 },
                 {
                   tabName: "添加会议室",
-                  tabIcon: AddCircleOutline
+                  tabIcon: AddCircleOutline,
+                  tabContent: <AddRoom />
                 },
                 {
                   tabName: "删除会议室",
-                  tabIcon: DeleteOutline
+                  tabIcon: DeleteOutline,
+                    tabContent:<Button>Stupid</Button>
                 },
-                  {
-                      tabName: "修改会议室",
-                      tabIcon: PanTool
-                  }
+                {
+                  tabName: "修改会议室",
+                  tabIcon: PanTool
+                }
               ]}
             />
           </GridItem>
