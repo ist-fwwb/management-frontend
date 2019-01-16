b5c5252fddb34fa350766b0af221ba9657c09eef
studentliying <540906886@qq.com>
2019-01-16 21:29:59 +0800

    beautify adding&searching meeting room pages

--- src/components/Room/AddRoom.jsx
+++ src/components/Room/AddRoom.jsx
@@ -6,6 +6,7 @@ import React from "react";
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import CustomInput from "components/CustomInput/CustomInput.jsx";
+import Button from "components/CustomButtons/Button.jsx";
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -14,6 +15,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import TextField from "@material-ui/core/es/TextField/TextField";
 
 
 const allDevices=["AIRCONDITIONER", ]
@@ -62,67 +64,52 @@ class AddRoom extends React.Component {
   };
 
   render() {
+    const {classes} = this.props;
     return (
       <GridContainer xs={20} sm={20} md={12}>
-        <GridItem xs={20} sm={20} md={12}>
-          <span style={{ marginLeft: "35%", fontSize: "18px" }}>房间号：</span>
+        <GridItem classxs={20} sm={20} md={12}>
+          <span style={{ marginLeft: "35%", fontSize: "20px", lineHeight:"100px" }}>房间号：</span>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <CustomInput
-              formControlProps={{
-                  className: "room-number"
-              }}
-              inputProps={{
-                placeholder: "请输入房间号",
-                inputProps: {
-                  "aria-label": "room"
-              }
-            }}
-          />
+            <TextField placeholder= "请输入房间号" variant="outlined" style={{width:"20%", fontSize: "20px", lineHeight:"100px"}} />
         </GridItem>
 
-          <h6 />
-
         <GridItem xs={20} sm={20} md={12}>
-          <span style={{ marginLeft: "35%", fontSize: "18px" }}>可容纳人数：</span>
+          <span style={{ marginLeft: "35%", fontSize: "20px", lineHeight:"100px"}}>可容纳人数：</span>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <Select style={{width:"12%"}} value={this.state.capacity} onChange={this.handleCapacityChange}>
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={10}>10</MenuItem>
-              <MenuItem value={15}>15</MenuItem>
-              <MenuItem value={20}>20</MenuItem>
-              <MenuItem value={25}>25</MenuItem>
-              <MenuItem value={30}>30</MenuItem>
+          <Select style={{width:"12%", fontSize:"20px", align:"center"}}   value={this.state.capacity} onChange={this.handleCapacityChange}>
+              <MenuItem value={5} style={{fontSize:"20px"}}>5</MenuItem>
+              <MenuItem value={10} style={{fontSize:"20px"}}>10</MenuItem>
+              <MenuItem value={15} style={{fontSize:"20px"}}>15</MenuItem>
+              <MenuItem value={20} style={{fontSize:"20px"}}>20</MenuItem>
+              <MenuItem value={25} style={{fontSize:"20px"}}>25</MenuItem>
+              <MenuItem value={30} style={{fontSize:"20px"}}>30</MenuItem>
           </Select>
         </GridItem>
-          <h1 />
+
         <GridItem xs={20} sm={20} md={12}>
-            <span style={{ marginLeft: "35%", fontSize: "18px" }}>设备：</span>
+            <span style={{ marginLeft: "35%", fontSize: "20px", lineHeight:"100px" }}>设备：</span>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <Checkbox checked={this.state.AirConditioner} onChange={this.handleDeviceChange('AirConditioner')} value="AirConditioner" />
-            <span style={{fontSize: "18px" }}>空调</span>
+            <span style={{fontSize: "20px" }}>空调</span>
             <Checkbox  style={{ marginLeft: "2%"}} checked={this.state.BlockBoard} onChange={this.handleDeviceChange('BlockBoard')} value="BlockBoard" />
-            <span style={{fontSize: "18px" }}>白板</span>
+            <span style={{fontSize: "20px" }}>白板</span>
             <Checkbox style={{ marginLeft: "2%"}} checked={this.state.Table} onChange={this.handleDeviceChange('Table')} value="Table"/>
-            <span style={{fontSize: "18px" }}>桌子</span>
+            <span style={{fontSize: "20px" }}>桌子</span>
             <Checkbox style={{ marginLeft: "2%"}} checked={this.state.Projector} onChange={this.handleDeviceChange('Projector')} value="Projector"/>
-            <span style={{fontSize: "18px" }}>投影仪</span>
+            <span style={{fontSize: "20px" }}>投影仪</span>
             <Checkbox style={{ marginLeft: "2%"}} checked={this.state.PowerSupply} onChange={this.handleDeviceChange('PowerSupply')} value="PowerSupply"/>
-            <span style={{fontSize: "18px" }}>电源</span>
+            <span style={{fontSize: "20px" }}>电源</span>
         </GridItem>
+
         <GridItem xs={20} sm={20} md={12}>
-            <span style={{ marginLeft: "35%", fontSize: "18px" }}>备注：</span>
+            <span style={{ marginLeft: "35%", fontSize: "20px", lineHeight:"100px" }}>备注：</span>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <CustomInput
-                formControlProps={{
-                    className: "room-number"
-                }}
-                inputProps={{
-                    placeholder: "请输入备注信息",
-                    inputProps: {
-                        "aria-label": "commet"
-                    }
-                }}
-            />
+            <TextField placeholder= "请输入备注信息" variant="outlined" style={{width:"20%"}}/>
+        </GridItem>
+
+          <GridItem > &nbsp; </GridItem>
+        <GridItem xs={20} sm={20} md={12}>
+            <Button style={{marginLeft:"43%", fontSize:"25px", lineHeight:"30px"}}>添加会议室</Button>
         </GridItem>
       </GridContainer>
     );
