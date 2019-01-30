e9d46d3a41646a5708df7cc4334dc3424117ec60
studentliying <540906886@qq.com>
2019-01-30 23:22:38 +0800

    add slides to HomePage

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -1,146 +1,144 @@
 import React from "react";
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
-import CustomTabs from "components/CustomTabs/CustomTabs.jsx";
-
-import Assignment from "@material-ui/icons/Assignment";
-import LibraryAdd from "@material-ui/icons/LibraryAdd";
-import FiberNew from "@material-ui/icons/FiberNew";
-import History from "@material-ui/icons/History";
-import Note from "@material-ui/icons/Note";
 import ErrorOutline from "@material-ui/icons/ErrorOutline";
 import Done from "@material-ui/icons/Done";
-
+import Slider from "react-slick";
 import Table from "components/Table/Table.jsx";
-import Button from "components/CustomButtons/Button.jsx";
 import Snackbar from "components/Snackbar/Snackbar.jsx";
-import { meetingController, today } from "variables/general.jsx";
+import { meetingController, idToTime, today } from "variables/general.jsx";
 import { Link } from "react-router-dom";
-import { idToTime } from "variables/general.jsx";
-import TodayMeeting from "../../components/Meeting/TodayMeeting";
+
+const slidesSettings = {
+    dots: true,
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+};
 
 const news = [
-  ["温州皮革厂倒闭了", "2018-01-20"],
-  ["温州皮革厂开业了", "2018-01-21"],
+    ["温州皮革厂倒闭了", "2018-01-20"],
+    ["温州皮革厂开业了", "2018-01-21"],
 ];
 
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            br: false,
+            notificationMessage: "null",
+            notificationType: null,
 
-class HomePage extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      br: false,
-      notificationMessage: "null",
-      notificationType: null,
-
-      meetings: null,
+            error: false
+        };
     }
-  }
-
-  JSONToArray = (jsonArray, type) => {
-    let re = [];
-    for (let i in jsonArray){
-      let ele = jsonArray[i];
-      if (type === "meeting" && ele.date !== today)
-        continue;
-      if (type === "meeting" && !(ele.status === "Pending" || ele.status === "Running"))
-        continue;
-      if (type === "attend" && (ele.status !== "Pending" || ele.hostId === this.props.userId))
-        continue;
-      let temp_ele = [];
-  
-      temp_ele.push(<Link to={"/meeting/"+ele.id+"/profile"}>{ele.heading}</Link>)
-      temp_ele.push(<Link to={"/room/"+ele.id+"/profile"}>{ele.location}</Link>);
-      if (type !== "meeting")
-        temp_ele.push(ele.date);
-      temp_ele.push(idToTime(ele.startTime) + "~" + idToTime(ele.endTime));
-      if (type === "meeting")
-        temp_ele.push(ele.status);
-      else if (type === "attend")
-        temp_ele.push([])
-      re.push(temp_ele);
-    }
-    return re;
-  }
 
 
-  showNotification = (place) => {
-    let x = [];
-    x[place] = true;
-    this.setState(x);
-    this.alertTimeout = setTimeout(
-      function() {
-        x[place] = false;
+
+    showNotification = place => {
+        let x = [];
+        x[place] = true;
         this.setState(x);
-      }.bind(this),
-      6000
-    );
-  }
+        this.alertTimeout = setTimeout(
+            function() {
+                x[place] = false;
+                this.setState(x);
+            }.bind(this),
+            6000
+        );
+    };
 
-  typeToIcon = (type) => {
-    if (type === "success")
-      return Done;
-    if (type === "danger")
-      return ErrorOutline;
-    return null;
-  }
+    typeToIcon = type => {
+        if (type === "success") return Done;
+        if (type === "danger") return ErrorOutline;
+        return null;
+    };
 
-  warning = (msg) => {
-    this.setState({
-      notificationType: "danger",
-      notificationMessage: msg
-    })
-    this.showNotification("br");
-  }
+    warning = msg => {
+        this.setState({
+            notificationType: "danger",
+            notificationMessage: msg
+        });
+        this.showNotification("br");
+    };
 
-  render(){
-    if (this.state.error)
-      return <h2>Network Error</h2>
-    let { meetings, attendMeetings, historyCancelledMeetings, historyStoppedMeetings } = this.state;
-    let historyMeetings = historyCancelledMeetings; 
-    console.log(historyMeetings)
-    let historyLoaded = false;
-    if ( historyCancelledMeetings && historyStoppedMeetings){
-      historyLoaded = true;
-      historyMeetings = historyMeetings.concat(historyStoppedMeetings);
-    }
-    return(
-      <div>
-        <GridContainer>
+    success = msg => {
+        this.setState({
+            notificationType: "success",
+            notificationMessage: msg
+        });
+        this.showNotification("br");
+    };
 
 
-          <GridItem xs={12} sm={12} md={12}>
-            <CustomTabs
-                title={null}
-                style={{background:"#000"}}
-                headerColor="danger"
-                tabs={[
-                    {
-                        tabName: "公司新闻",
-                        tabIcon: FiberNew,
-                        tabContent: (
+    render() {
+        if (this.state.error) return <h2>Network Error</h2>;
+        return (
+            <div>
+                <GridContainer xs={12} sm={12} md={12}>
+                    <GridItem xs={12} sm={12} md={8}>
+                        <Slider {...slidesSettings} style={{width:"50%"}}>
+                            <div
+                                style={{
+                                    maxWidth: "100%",
+                                    height: "30%",
+                                    paddingBottom: "40%",
+                                    overflow: "hidden"
+                                }}
+                            >
+                                <img
+                                    width="100%"
+                                    alt="img"
+                                    style={{height:"400px"}}
+                                    src={
+                                        "https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                                    }
+                                />
+                                <h5 style={{align:"center"}}>{news[0][1]}  {news[0][0]}</h5>
+                            </div>
+                            <div
+                                style={{
+                                    maxWidth: "100%",
+                                    height: "30%",
+                                    paddingBottom: "40%",
+                                    overflow: "hidden"
+                                }}
+                            >
+                                <img
+                                    width="100%"
+                                    style={{height:"400px"}}
+                                    alt="img"
+                                    src={
+                                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                                    }
+                                />
+                                <h5 style={{align:"center"}}>{news[1][1]}  {news[1][0]}</h5>
+                            </div>
+                        </Slider>
+                    </GridItem>
+                        <GridItem xs={12} sm={12} md={4}>
                             <Table
                                 tableHeaderColor="primary"
                                 tableHead={["标题", "日期"]}
                                 tableData={news}
                             />
-                        )
-                    }
-                ]}
-            />
-          </GridItem>
-        </GridContainer>
-        <Snackbar
-          place="br"
-          color={this.state.notificationType}
-          icon={this.typeToIcon(this.state.notificationType)}
-          message={this.state.notificationMessage}
-          open={this.state.br}
-          closeNotification={() => this.setState({ br: false })}
-          close
-        />
-      </div>
-    )
-  }
+                        </GridItem>
+                </GridContainer>
+
+                <Snackbar
+                    place="br"
+                    color={this.state.notificationType}
+                    icon={this.typeToIcon(this.state.notificationType)}
+                    message={this.state.notificationMessage}
+                    open={this.state.br}
+                    closeNotification={() => this.setState({ br: false })}
+                    close
+                />
+            </div>
+        );
+    }
 }
 export default HomePage;
